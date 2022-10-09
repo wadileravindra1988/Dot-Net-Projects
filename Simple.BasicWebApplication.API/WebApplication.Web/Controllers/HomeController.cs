ac35e3d9ea7582403f6c19aaa828960d961a9ed3
wadileravindra1988 <wadileravindra1988@gmail.com>
2022-10-09 20:00:43 +0530

    MVC Crud all operation completed

--- Simple.BasicWebApplication.API/WebApplication.Web/Controllers/HomeController.cs
+++ Simple.BasicWebApplication.API/WebApplication.Web/Controllers/HomeController.cs
@@ -9,7 +9,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using WebApplication.Web.Models;
-
 namespace WebApplication.Web.Controllers
 {
     public class HomeController : Controller
@@ -35,6 +34,79 @@ namespace WebApplication.Web.Controllers
             return View(employees);
         }
 
+        public async Task<IActionResult> Details(int id)
+        {
+            Employee employees = await GetEmployeeByID(id);
+            return View(employees);
+        }
+
+        private static async Task<Employee> GetEmployeeByID(int id)
+        {
+            Employee employees = new Employee();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:5001/");
+            HttpResponseMessage response = await client.GetAsync($"api/employees/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                employees = JsonConvert.DeserializeObject<Employee>(result);
+            }
+
+            return employees;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:5001/");
+            var response = await client.PostAsJsonAsync<Employee>("api/employees", employee);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            Employee employees = new Employee();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:5001/");
+            HttpResponseMessage response = await client.DeleteAsync($"api/employees/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            Employee employees = await GetEmployeeByID(id);
+            return View(employees);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Employee employee)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:5001/");
+            var response = await client.PutAsJsonAsync<Employee>($"api/employees/{employee.Id}", employee);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
